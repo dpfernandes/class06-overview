12862cc719e20a5428c4dc62ce7ee728c0d981b9
Daniel Fernandes <daniel.pastor.fernandes@gmail.com>
2018-11-16 16:23:03 +0100

    img resize

--- src/CardHeader.js
+++ src/CardHeader.js
@@ -5,7 +5,7 @@ class CardHeader extends Component {
     render() {  
       return (
         <div>
-          <img src={this.props.image}  width="250px" alt={''} />
+          <img src={this.props.image}  width="280px" alt={''} />
         </div>
       )
     }
