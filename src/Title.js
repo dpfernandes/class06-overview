3ec81cdefa558ad027696d079aecba2be6096e87
Daniel Fernandes <daniel.pastor.fernandes@gmail.com>
2018-11-18 10:50:36 +0100

    logo

--- src/Title.js
+++ src/Title.js
@@ -1,9 +1,11 @@
 import React from "react";
+import hyfDKlogo from './images/hyf_dk_logo.png';
 
 class Title extends React.Component {
     render() {
       return <section className="app-title">
         <div className="app-title-content">
+          <img src={hyfDKlogo} className="hfDKlogo"   width="80px" alt="Hack Your Future"/>
           <h1>Class06 Overview</h1>
         </div>
       </section>
